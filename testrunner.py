e4be60a9c66aab2a1c8ac5d4b79af234c276a19a
Jakub Kaczmarzyk <jakubk@mit.edu>
2017-12-06 10:14:23 -0500

    run workflow only if file is run as __main__

--- testrunner.py
+++ testrunner.py
@@ -4,13 +4,12 @@ import pdb
 
 from workflowregtest import WorkflowRegtest
 
-Workflow_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)),  "workflows4regtests", "basic_examples",)
 
+if __name__ == "__main__":
+    Workflow_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)),  "workflows4regtests", "basic_examples",)
 
-for workflow in next(os.walk(Workflow_dir))[1]:
-    wf = WorkflowRegtest(os.path.join(Workflow_dir, workflow))
-    wf.docker_images()
-    wf.testing_workflow()
-
-                         
-    
+    for workflow in next(os.walk(Workflow_dir))[1]:
+        wf = WorkflowRegtest(os.path.join(Workflow_dir, workflow))
+        wf.generate_dockerfiles()
+        wf.build_images()
+        wf.testing_workflow()
