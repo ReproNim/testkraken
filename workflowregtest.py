0e675aa173d82a4c44efadd7dae8d17dedd932ff
Jakub Kaczmarzyk <jakubk@mit.edu>
2017-12-06 10:13:35 -0500

    add method to build docker images

--- workflowregtest.py
+++ workflowregtest.py
@@ -1,9 +1,13 @@
-import os, subprocess, json 
-import tempfile
+"""Object to orchestrate worflow execution."""
+
 import itertools
-import pdb
+import json
+import os
+import subprocess
+import tempfile
 
 import container_generator as cg
+
 
 class WorkflowRegtest(object):
     def __init__(self, workflow_path):
@@ -17,9 +21,13 @@ class WorkflowRegtest(object):
         self.tests = self.parameters["tests"] # should be a tuple (output_name, test_name)
         self.software_names, self.software_vers_gen = self.environment_map()
 
+        self.tmpdir = tempfile.TemporaryDirectory(
+            prefix="tmp-workflowregtest-", dir=os.getcwd()
+        )
+
 
     #TODO we double work with Jakub, should choose one
-    # or at least to be sure taht the order is correct    
+    # or at least to be sure taht the order is correct
     def environment_map(self):
         software_name =[]
         software_version = []
@@ -45,61 +53,70 @@ class WorkflowRegtest(object):
 
 
     # TODO, just copied from Jakub example
-    def docker_images(self):
+    def generate_dockerfiles(self):
         matrix = cg.create_matrix_of_envs(self.parameters['env'])
-        mapping = cg.get_dict_of_neurodocker_dicts(matrix)
-        tmpdir = tempfile.TemporaryDirectory(prefix="tmp-json-files-",                                
-                                             dir=os.getcwd())
-        os.mkdir(os.path.join(tmpdir.name, 'json'))
-        keep_tmpdir = True
+        self.mapping = cg.get_dict_of_neurodocker_dicts(matrix)
+
+        os.mkdir(os.path.join(self.tmpdir.name, 'json'))
         try:
-            for sha1, neurodocker_dict in mapping.items():
+            for sha1, neurodocker_dict in self.mapping.items():
                 print("building images: {}".format(neurodocker_dict))
-                cg.generate_dockerfile(tmpdir.name, neurodocker_dict, sha1)
+                cg.generate_dockerfile(
+                    self.tmpdir.name, neurodocker_dict, sha1
+                )
         except Exception as e:
             raise
 
-    
+    def build_images(self):
+        for sha1 in self.mapping:
+            filepath = os.path.join(
+                self.tmpdir.name, 'Dockerfile.{}'.format(sha1)
+            )
+            tag = "repronim/regtests:{}".format(sha1)
+            cg.build_image(filepath, build_context=None, tag=tag)
+
     def run_cwl(self, image):
         self.creating_cwl(image)
         self.creating_cwl_input()
         subprocess.call(["cwl-runner", "cwl.cwl", "input.yml"])
 
-
     def creating_cwl(self, image):
-        cmd_cwl = ("#!/usr/bin/env cwl-runner\n"
-        "cwlVersion: v1.0\n"
-        "class: CommandLineTool\n"
-        "baseCommand: {}\n"
-        "hints:\n"
-        "  DockerRequirement:\n"
-        "    dockerPull: {}\n"
-        "inputs:\n" 
-        "  script:\n" 
-        "    type: File\n"
-        "    inputBinding:\n"
-        "      position: 1\n\n"
-        "outputs:\n").format(self.command, image) #TODO
+        cmd_cwl = (
+            "#!/usr/bin/env cwl-runner\n"
+            "cwlVersion: v1.0\n"
+            "class: CommandLineTool\n"
+            "baseCommand: {}\n"
+            "hints:\n"
+            "  DockerRequirement:\n"
+            "    dockerPull: {}\n"
+            "inputs:\n"
+            "  script:\n"
+            "    type: File\n"
+            "    inputBinding:\n"
+            "      position: 1\n\n"
+            "outputs:\n"
+        ).format(self.command, image) #TODO
 
         for (ii, test_tuple) in enumerate(self.tests):
-            cmd_cwl += ("  output_files_{}:\n"
-            "    type: File\n"
-            "    outputBinding:\n"
-            "      glob: {}\n").format(ii, test_tuple[0])
-
+            cmd_cwl += (
+                "  output_files_{}:\n"
+                "    type: File\n"
+                "    outputBinding:\n"
+                "      glob: {}\n"
+            ).format(ii, test_tuple[0])
 
         with open("cwl.cwl", "w") as cwl_file:
             cwl_file.write(cmd_cwl)
 
-
     def creating_cwl_input(self):
-        cmd_in = ("script:\n" 
-        "  class: File\n"
-        "  path: {}\n").format(self.script)
-        
+        cmd_in = (
+            "script:\n"
+            "  class: File\n"
+            "  path: {}\n"
+        ).format(self.script)
+
         with open("input.yml", "w") as inp_file:
             inp_file.write(cmd_in)
-
 
     def run_tests(self):
         for (output, test) in self.tests:
@@ -107,6 +124,3 @@ class WorkflowRegtest(object):
             getattr(testing_module, test)(output, os.path.join(self.workflow_path, "data_ref", output))
             # either use pytest.main() https://docs.pytest.org/en/latest/usage.html#calling-pytest-from-python-code
             # or another cwl runner
-
-                         
-    
