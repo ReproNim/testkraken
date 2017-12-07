1563f4f8e0886d6f41b9fa59dbc10ed656585ed9
Dorota Jarecka <djarecka@gmail.com>
2017-12-07 10:21:01 -0500

    removing numpy for now

--- testing_functions/test_rand_almosteq.py
+++ testing_functions/test_rand_almosteq.py
@@ -3,19 +3,18 @@
 import os, json
 import inspect
 import random
-import numpy as np
+#import numpy as np
 
-def test_random_numbers(file_out, file_ref, report_txt):
+def test_rand_almosteq(file_out, file_ref, report_txt):
     with open(file_out) as f:
         res_out = json.load(f)
     with open(file_ref) as f:
         res_ref = json.load(f)
 
-    random.seed(3)
-    np.testing.assert_almost_equal(res_out, res_ref)
-
     try:
-        assert list_out == list_ref
+        #np.testing.assert_almost_equal(res_out, res_ref)
+        assert abs(res_out - res_ref) < 0.1
+
         report_txt.write("Test: {}, OutputFile: {}: PASSED\n".format(inspect.stack()[0][3],
                                                                      file_out))
 
--- workflows4regtests/basic_examples/pseudo_random_numbers/workflow/mean_random_numbers.py
+++ workflows4regtests/basic_examples/pseudo_random_numbers/workflow/mean_random_numbers.py
@@ -1,11 +1,18 @@
 import json
 import random
-import numpy as np
+#import numpy as np
 
-def mean_random_numbers():
+def mean_random_numbers(k):
     """a function that returns the mean absolute value of the list of pseudo-random numbers"""
+    random.seed(k)
     a = random.sample(range(-100, 100), 10)
-    mean = np.absolute(a).mean()
+    a_abs = [abs(int(i)) for i in a]
+
+    mean = sum(a_abs) / len(a_abs)
+    #mean = np.absolute(a).mean()
 
     with open('mean.json', 'w') as outfile:
         json.dump(mean, outfile)
+
+if __name__ == '__main__':
+    mean_random_numbers(2)
