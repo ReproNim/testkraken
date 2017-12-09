1ba44150f211533874da6e6fb67a18ed5728d490
Dorota Jarecka <djarecka@gmail.com>
2017-12-09 15:13:32 -0500

    adding command line interface to tests so i can use it with cwl; changing to report_filename (not open file) for reports, so new file will be created for every test (at least temprorary, not sure how to provide as an input and output a File in cwl)

--- testing_functions/test_obj_eq.py
+++ testing_functions/test_obj_eq.py
@@ -3,7 +3,7 @@
 import os, json
 import inspect
 
-def test_obj_eq(file_out, file_ref, report_txt):
+def test_obj_eq(file_out, file_ref, report_filename):
     with open(file_out) as f:
         list_out = json.load(f)
     with open(file_ref) as f:
@@ -11,10 +11,26 @@ def test_obj_eq(file_out, file_ref, report_txt):
 
     try:
         assert list_out == list_ref
-        report_txt.write("Test: {}, OutputFile: {}: PASSED\n".format(inspect.stack()[0][3],
-                                                                     file_out))
+        with open(report_filename, "a") as f:
+            f.write("Test: {}, OutputFile: {}: PASSED\n".format(inspect.stack()[0][3],
+                                                                os.path.basename(file_out)))
 
     except(AssertionError):
-        report_txt.write("Test: {}, OutputFile: {}: FAILED\n".format(inspect.stack()[0][3],
-                                                                     file_out))
+        with open(report_filename, "a") as f:
+            f.write("Test: {}, OutputFile: {}: FAILED\n".format(inspect.stack()[0][3],
+                                                                os.path.basename(file_out)))
 
+
+if __name__ == '__main__':
+    from argparse import ArgumentParser, RawTextHelpFormatter
+    parser = ArgumentParser(description=__doc__,
+                            formatter_class=RawTextHelpFormatter)
+    parser.add_argument("-out", dest="file_out",
+                        help="file with the output for testing")
+    parser.add_argument("-ref", dest="file_ref",
+                        help="file with the reference output")
+    parser.add_argument("-report", dest="report_filename",
+                        help="file to save tests output")
+    args = parser.parse_args()
+
+    test_obj_eq(**vars(args))
