04c8f8864cfaacb2845cf815465b2134bc9f5013
Dorota Jarecka <djarecka@gmail.com>
2017-12-15 13:21:31 -0500

    testing only some diectories for testinng

--- testrunner.py
+++ testrunner.py
@@ -11,7 +11,8 @@ Workflows_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)),
 
 if __name__ == "__main__":
     for workflow in next(os.walk(Workflows_dir))[1]:
-        wf = WorkflowRegtest(os.path.join(Workflows_dir, workflow))
-        wf.generate_dockerfiles()
-        wf.build_images()
-        wf.testing_workflow()
+        if "env" in workflow or "sort" in workflow:
+            wf = WorkflowRegtest(os.path.join(Workflows_dir, workflow))
+            wf.generate_dockerfiles()
+            wf.build_images()
+            wf.testing_workflow()
