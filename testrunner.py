cd2b5e2e74e9612ae994a68e1055479bbadee12c
Dorota Jarecka <djarecka@gmail.com>
2018-03-28 00:11:21 -0400

    merging all results to one csv file, adding a temp plt method to plot all results on one plotly parcoords

--- testrunner.py
+++ testrunner.py
@@ -13,5 +13,8 @@ if __name__ == "__main__":
     for workflow in next(os.walk(Workflows_dir))[1]:
         wf = WorkflowRegtest(os.path.join(Workflows_dir, workflow))
         wf.run()
+        wf.merging_all_output()
+        # TODO: these method will be removed
+        wf.plot_all_results_paralcoord()
         wf.merging_output()
         wf.plot_workflow_result_paralcoord()
--- workflowregtest.py
+++ workflowregtest.py
@@ -27,6 +27,7 @@ class WorkflowRegtest(object):
                                    self.parameters["script"])
         self.command = self.parameters["command"] # TODO: adding arg
         self.tests_regr = self.parameters["tests_regr"] # should be a tuple (output_name, test_name)
+        self.tests_stat = self.parameters["tests_stat"]
         self.inputs = self.parameters["inputs"]
         self.tmpdir = tempfile.TemporaryDirectory(
             prefix="tmp-workflowregtest-", dir=os.getcwd()
@@ -100,6 +101,47 @@ class WorkflowRegtest(object):
         self._testing_workflow()
 
 
+    def merging_all_output(self):
+        self.res_all = []
+        no_docker = []
+        ii_ok = None # just to have at least one env that docker was ok
+        for ii, soft_d in enumerate(self.matrix_envs_dict):
+            #self.res_all.append(deepcopy(soft_d))
+            el_dict = deepcopy(soft_d)
+            if self.docker_status[ii] == "docker ok":
+                ii_ok = ii
+                for (iir, regr) in enumerate(self.tests_regr):
+                    file_name = "report_regr_{}_{}_{}.txt".format(iir, os.path.basename(self.workflow_path),
+                                                                  self.soft_str[ii])
+                    with open(file_name) as f:
+                        f_txt = f.read()
+                        res = f_txt.split()[-1] # TODO: this will be changed
+                        el_dict["regr_{}".format(iir)] = res
+
+                for (iis, stat) in enumerate(self.tests_stat):
+                    file_name = "report_stat_{}_{}_{}.txt".format(iis, os.path.basename(self.workflow_path),
+                                                                  self.soft_str[ii])
+                    with open(file_name) as f:
+                        f_dict = json.load(f)
+                        for key, res in f_dict.items():
+                                el_dict["stat_{}:{}".format(iis, key)] = res
+            else:
+                no_docker.append(ii)
+            self.res_all.append(el_dict)
+
+        self.res_keys = self.res_all[ii_ok].keys() - soft_d.keys()
+        if ii_ok and no_docker:
+            for ii in no_docker:
+                for key in self.res_keys:
+                    self.res_all[ii][key] = "N/A"
+
+        keys_csv = self.res_all[0].keys()
+        with open("{}_output_all.csv".format(os.path.basename(self.workflow_path)), 'w') as outfile:
+            csv_writer = csv.DictWriter(outfile, keys_csv)
+            csv_writer.writeheader()
+            csv_writer.writerows(self.res_all)
+
+
     def merging_output(self):
         self.res_dict = []
         for ii, test in enumerate(self.tests_regr):
@@ -148,6 +190,41 @@ class WorkflowRegtest(object):
             csv_writer.writerows(self._res_list)
 
 
+    def plot_all_results_paralcoord(self):
+        import pandas
+        import plotly.graph_objs as go
+        from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
+
+        df = pandas.DataFrame(self.res_all)
+
+        list_pl = []
+        for i, k in self.env_parameters.items():
+            soft_values = df[i]
+            soft_values = soft_values.replace(k, list(range(len(k))))
+            list_pl.append(dict(label=i, values=soft_values, tickvals=list(range(len(k))), ticktext=k ))
+
+        for key in self.res_keys:
+            if "reg" in key:
+                reg_values = df[key]
+                reg_values = reg_values.replace(["PASSED", "FAILED", "N/A"], [0, 1, 2])
+                list_pl.append(dict(label=key, values=reg_values,
+                                    tickvals=[0, 1, 2], ticktext=["passed", "failed", "N/A"]))
+            else:
+                stat_values = df[key]
+                stat_values = stat_values.replace(["N/A"], [-999])
+                list_pl.append(dict(label=key, values=stat_values))
+
+        data = [go.Parcoords(line=dict(color = 'blue'), dimensions=list_pl)]
+
+        layout = go.Layout(
+            plot_bgcolor='#E5E5E5',
+            paper_bgcolor='#E5E5E5'
+        )
+
+        fig = go.Figure(data=data, layout = layout)
+        plot(fig, filename='parcoords_{}_All'.format(os.path.basename(self.workflow_path)))
+
+
     def plot_workflow_result_paralcoord(self):
         for ii, test in enumerate(self.tests_regr): #will probably use also name later
             self._plot_workflow_result_paralcoord_test(ii)
@@ -155,7 +232,6 @@ class WorkflowRegtest(object):
     def _plot_workflow_result_paralcoord_test(self, test_id):
         """plotting results, this has to be cleaned TODO"""
         import pandas
-        import plotly.plotly as py
         import plotly.graph_objs as go
         from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
 
