751d9b311a1a3a3da65c34bc7445a033ada417e9
Dorota Jarecka <djarecka@gmail.com>
2018-04-02 14:50:44 -0400

    removing older merging and plotting method that were not being updated and are commented

--- workflowregtest.py
+++ workflowregtest.py
@@ -144,55 +144,6 @@ class WorkflowRegtest(object):
             csv_writer.writerows(self.res_all)
 
 
-    def merging_output(self):
-        # TODO: not used, changes not finished, since will be removed
-        self.res_dict = []
-        for ii, test in enumerate(self.tests):
-            self.res_dict.append(OrderedDict())
-            self._merging_output_test(ii)
-
-
-    def _merging_output_test(self, test_id):
-        # creating a list with results, each element has a dict with soft desc. and result
-        self._res_list = []
-        # create dictionary env: list for all env desc. (indexes from self.env_parameters),
-        # and results: list of results for all env
-        for key in self.env_parameters:
-            self.res_dict[test_id][key] = []
-        self.res_dict[test_id]["result"] = []
-
-        for ii, soft_d in enumerate(self.matrix_envs_dict):
-            el_dict = deepcopy(soft_d)
-            file_name = "report_regr_{}_{}_{}.txt".format(test_id, os.path.basename(self.workflow_path),
-                                                     self.soft_str[ii])
-            for k, val in soft_d.items():
-                self.res_dict[test_id][k].append(self.env_parameters[k].index(val))
-
-            if self.docker_status[ii] == "docker ok":
-                with open(file_name) as f:
-                    f_txt = f.read()
-                    if "PASS" in f_txt:
-                        el_dict["result"] = "PASS"
-                        self.res_dict[test_id]["result"].append("1")
-                    elif "FAIL" in f_txt:
-                        el_dict["result"] = "FAIL"
-                        self.res_dict[test_id]["result"].append("0")
-                    else:
-                         el_dict["result"] = "N/A"
-                         self.res_dict[test_id]["result"].append("2")
-            else:
-                 el_dict["result"] = "N/A"
-                 self.res_dict[test_id]["result"].append("2")
-            self._res_list.append(el_dict)
-
-        # saving merged results in one csv file
-        keys_csv = self._res_list[0].keys()
-        with open("{}_output_{}.csv".format(os.path.basename(self.workflow_path), test_id), 'w') as outfile:
-            csv_writer = csv.DictWriter(outfile, keys_csv)
-            csv_writer.writeheader()
-            csv_writer.writerows(self._res_list)
-
-
     def plot_all_results_paralcoord(self):
         import pandas
         import plotly.graph_objs as go
@@ -229,36 +180,3 @@ class WorkflowRegtest(object):
 
         fig = go.Figure(data=data, layout = layout)
         plot(fig, filename=os.path.join(self.working_dir,'parcoords_{}_All'.format(os.path.basename(self.workflow_path))))
-
-
-    def plot_workflow_result_paralcoord(self):
-        # TODO: not used, changes not finished, since will be removed
-        for ii, test in enumerate(self.tests_regr): #will probably use also name later
-            self._plot_workflow_result_paralcoord_test(ii)
-
-    def _plot_workflow_result_paralcoord_test(self, test_id):
-        """plotting results, this has to be cleaned TODO"""
-        import pandas
-        import plotly.graph_objs as go
-        from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
-
-        df = pandas.DataFrame(self.res_dict[test_id])
-
-        list_pl = []
-        for i, k in self.env_parameters.items():
-            list_pl.append(dict(label=i, values=df[i], tickvals=list(range(len(k))), ticktext=k ))
-        list_pl.append(dict(label="result", values=df["result"],
-                            tickvals=[0, 1, 2], ticktext=["fail", "pass", "N/A"]))
-        colors_d = {'0': "red", '1': "green", '2': "black"}
-        my_colorscale =[]
-        for ii in set(df["result"]):
-            my_colorscale.append([ii, colors_d[ii]])
-        line_pl = dict(color=df["result"], colorscale = my_colorscale)
-        data = [go.Parcoords(line=line_pl, dimensions=list_pl)]
-        layout = go.Layout(
-            plot_bgcolor='#E5E5E5',
-            paper_bgcolor='#E5E5E5'
-        )
-
-        fig = go.Figure(data=data, layout = layout)
-        plot(fig, filename='parcoords_{}_{}'.format(os.path.basename(self.workflow_path), test_id))
