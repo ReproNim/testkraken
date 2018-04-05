81b6a5ce93bbdc0e232a16382f546e7cd9462898
Dorota Jarecka <djarecka@gmail.com>
2018-04-05 07:53:45 -0400

    choosing some columns only

--- testing_functions/check_output.py
+++ testing_functions/check_output.py
@@ -45,10 +45,10 @@ def check_output(file_out, file_ref=None, name=None, **kwargs):
     out = {}
 
     for key in df_exp.columns:
-        if ("voxels" in key) and ("Right" not in key):
+        if key in ["white", "gray", "csf", "Right-Hippocampus", "Right-Amygdala", "Right-Caudate"]:
             if df_exp[key].values[0] != 0.:
                 out["diff:{}".format(key.replace("_voxels", ""))] = round(
-                    1. * abs(df_exp[key].values[0] - df_out[key].values[0]) / df_exp[key].values[0], 3)
+                    1. * abs(df_exp[key].values[0] - df_out[key].values[0]) / df_exp[key].values[0], 5)
             elif df_out[key].values[0] != 0.:
                 out["diff:{}".format(key.replace("_voxels", ""))] = 1.
             else:
