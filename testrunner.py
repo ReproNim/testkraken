22e20a98d2b764de1a84846f8d286be7ac1772bc
Dorota Jarecka <djarecka@gmail.com>
2018-04-05 12:21:08 -0400

    running all workflows

--- testrunner.py
+++ testrunner.py
@@ -11,11 +11,10 @@ Workflows_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)),
 
 if __name__ == "__main__":
     for workflow in next(os.walk(Workflows_dir))[1]:
-        if "list" in workflow:
-            print("Workflow Name ", workflow)
-            wf = WorkflowRegtest(os.path.join(Workflows_dir, workflow))
-            wf.run()
-            wf.merging_all_output()
-            # # TODO: this method will be removed
-            #wf.plot_all_results_paralcoord()
-            wf.dashboard_workflow()
+        print("Workflow Name ", workflow)
+        wf = WorkflowRegtest(os.path.join(Workflows_dir, workflow))
+        wf.run()
+        wf.merging_all_output()
+        # # TODO: this method will be removed
+        #wf.plot_all_results_paralcoord()
+        wf.dashboard_workflow()
