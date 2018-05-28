b070758bbaa7a3e4be218f5b5603b32cb9913310
Dorota Jarecka <djarecka@gmail.com>
2018-05-28 19:58:34 -0400

    adding an additional plot for scattering two values; im using now for plotting both self.res_all_df, self.res_all_flat_df (probably should be changed)

--- altair_plots.py
+++ altair_plots.py
@@ -5,13 +5,15 @@ import pandas as pd
 
 
 class AltairPlots(object):
-    def __init__(self, wf_dir, results_df, plot_spec):
+    def __init__(self, wf_dir, results_df, results_df_flat, plot_spec):
         self.wf_dir = wf_dir
         self.index = os.path.join(self.wf_dir, "index.html")
         self.plot_spec = plot_spec
         self._soup = self._index_read()
         self.results = results_df
+        self.results_flat = results_df_flat
         self.plot_description = {"scatter_all": "scatter plot for all environments",
+                                 "scatter_2var": "scatter two tests results for all environments",
                                  "barplot_all": "barplot for all environments",
                                  "barplot_all_rel_error": "barplot of the relative errors for all environments"}
         self.drop_key_list_default = ["python", "base", "fsl", "conda_install", "pip_install"]
@@ -31,8 +33,8 @@ class AltairPlots(object):
     def scatter_all(self, var_l):
         if not var_l:
             # if var_l is not provided, I will plot all
-            var_l = [col for col in self.results.columns if col not in self.drop_key_list_default]
-        res_plot = self.results[var_l].fillna("NaN")
+            var_l = [col for col in self.results_flat.columns if col not in self.drop_key_list_default]
+        res_plot = self.results_flat[var_l].fillna("NaN")
         res_transf = res_plot.reset_index().melt(['env', 'index'])
         plot_dict = alt.Chart(res_transf).mark_circle(
                 size=50, opacity=0.5).encode(
@@ -43,15 +45,30 @@ class AltairPlots(object):
         return plot_dict
 
 
+    def scatter_2var(self, var_scat):
+        plot = []
+        for var in var_scat:
+            res_plot = self.results[var + ["env"]].fillna("NaN")
+            plot_el = alt.Chart(res_plot).mark_circle(
+                size=50, opacity=0.5).encode(
+                x='{}:Q'.format(var[0]),
+                y='{}:Q'.format(var[1]),
+                color='env:N').properties(
+                width=400).interactive()
+            plot.append(plot_el)
+        plot_dict = alt.hconcat(*plot).properties(background="#a9a3b7").to_dict()
+        return plot_dict
+
+
     def barplot_all(self, var_l, y_scale=None, y_max_check=False, default_col="test"):
         if not var_l:
             # if var_l is not provided, I will plot all
-            var_l = [col for col in self.results.columns if default_col in col]
-        res_plot = self.results[var_l+['env']].fillna("NaN")
+            var_l = [col for col in self.results_flat.columns if default_col in col]
+        res_plot = self.results_flat[var_l+['env']].fillna("NaN")
         res_transf = res_plot.reset_index().melt(['env', 'index'])
         if y_scale: #should be a tuple, TODO
             if y_max_check:
-                y_max = round(self.results[var_l].max().max() + .1, 2)
+                y_max = round(self.results_flat[var_l].max().max() + .1, 2)
                 y_scale_update = (y_scale[0], y_max)
                 y_bar = alt.Y('value:Q', scale=alt.Scale(domain=y_scale_update))
             else:
@@ -65,7 +82,7 @@ class AltairPlots(object):
                 color='env:N').properties(
                 width=400)
         chart = alt.hconcat().properties(background="#a9a3b7")
-        for env in [ee for ee in self.results.env if ee != "N/A"]:
+        for env in [ee for ee in self.results_flat.env if ee != "N/A"]:
             chart |= base.transform_filter(alt.expr.datum.env == env)
         plot_dict = chart.to_dict()
         return plot_dict
