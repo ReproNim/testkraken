72beca87d56b245177663ac03c283ac7e264929f
Jakub Kaczmarzyk <jakubk@mit.edu>
2019-01-02 21:14:28 -0500

    commit to trigger circleci

--- testkraken/cli.py
+++ testkraken/cli.py
@@ -6,3 +6,5 @@ import click
 @click.argument('path', type=click.Path(exists=True))
 def main(path):
     print("Not implemented yet. In the future, will run from {}".format(path))
+
+# QUESTION(kaczmarj): what commands should be available?
