1bdb4259392ae6b37cbd7a4cac61b3e6ade6740f
Dorota Jarecka <djarecka@gmail.com>
2019-01-06 18:31:30 -0500

    testing simple only if py=37

--- testkraken/tests/test_basic_examples.py
+++ testkraken/tests/test_basic_examples.py
@@ -5,7 +5,8 @@ from ..testrunner import runner
 
 Workflows_main_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)),
                                 "../../workflows4regtests", "basic_examples",)
-workflows_list = [os.path.join(Workflows_main_dir, workf) for workf in next(os.walk(Workflows_main_dir))[1]]
+workflows_list = [os.path.join(Workflows_main_dir, workf) for workf in next(os.walk(Workflows_main_dir))[1]
+                  if "simple" in "workf"]
 
 @pytest.mark.parametrize("workflow_dir", workflows_list)
 def test_basic_examples(workflow_dir):
