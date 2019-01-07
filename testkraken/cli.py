3b53949efec130a0418a36a62f44240c00ec3f04
Jakub Kaczmarzyk <jakubk@mit.edu>
2019-01-07 14:57:20 -0500

    update cli

--- testkraken/cli.py
+++ testkraken/cli.py
@@ -2,9 +2,14 @@
 
 import click
 
+from testkraken.workflowregtest import WorkflowRegtest
+
+
 @click.command(help="Run the workflow in PATH.")
 @click.argument('path', type=click.Path(exists=True))
-def main(path):
-    print("Not implemented yet. In the future, will run from {}".format(path))
-
-# QUESTION(kaczmarj): what commands should be available?
+@click.option('-w', '--working-dir', type=click.Path(), help="Working directory of workflow. Default is a temporary directory.")
+def main(path, working_dir=None):
+    wf = WorkflowRegtest(path, working_dir=working_dir)
+    wf.run()
+    wf.merging_all_output()
+    wf.dashboard_workflow()
