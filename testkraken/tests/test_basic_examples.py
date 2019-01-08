40e3b8b98b77d0a92ef8a66bbc2bdc403ba2ffae
Jakub Kaczmarzyk <jakubk@mit.edu>
2019-01-08 12:59:19 -0500

    fix runner

--- testkraken/tests/test_basic_examples.py
+++ testkraken/tests/test_basic_examples.py
@@ -1,17 +1,20 @@
 import os, pdb
 import pytest
 
-from ..testrunner import runner
+from testkraken.workflowregtest import WorkflowRegtest
 
 Workflows_main_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)),
                                 "../../workflows4regtests", "basic_examples",)
 workflows_list = [os.path.join(Workflows_main_dir, workf) for workf in next(os.walk(Workflows_main_dir))[1]]
 
-@pytest.mark.parametrize("workflow_dir", workflows_list)
+@pytest.mark.parametrize("workflow_dir", ["/code/regtests/workflows4regtests/basic_examples/simple_workflow"])
 def test_basic_examples(workflow_dir):
     print(workflow_dir)
     try:
-        runner(workflow_dir)
+        wf = WorkflowRegtest(workflow_dir)
+        wf.run()
+        wf.merge_outputs()
+        wf.dashboard_workflow()
     except Exception as e:
         print(e)
         assert False
