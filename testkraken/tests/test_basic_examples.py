10933a4f6e4d606658cc79e47500c331f552edd9
Jakub Kaczmarzyk <jakubk@mit.edu>
2019-01-08 14:19:10 -0500

    test all workflows

--- testkraken/tests/test_basic_examples.py
+++ testkraken/tests/test_basic_examples.py
@@ -7,7 +7,7 @@ Workflows_main_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)),
                                 "../../workflows4regtests", "basic_examples",)
 workflows_list = [os.path.join(Workflows_main_dir, workf) for workf in next(os.walk(Workflows_main_dir))[1]]
 
-@pytest.mark.parametrize("workflow_dir", ["/code/regtests/workflows4regtests/basic_examples/simple_workflow"])
+@pytest.mark.parametrize("workflow_dir", workflows_list)
 def test_basic_examples(workflow_dir):
     print(workflow_dir)
     try:
