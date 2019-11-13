40a7e87ab0eaab300215ea8cd802a7d33bb7b123
Dorota Jarecka <djarecka@gmail.com>
2019-11-13 15:00:37 -0500

    adding a simple test for afni, changing testkrken that allows for no script (just a bash command)

---
+++ testkraken/testing_functions/test_afni_scan_headers.py
@@ -0,0 +1,37 @@
+"""Checking if lists from two json files are equal"""
+from __future__ import division
+import json
+import nibabel as nib
+from pathlib import Path
+
+
+def test_afni_scan_header(file_out, file_ref=None, name=None):
+
+    image_out = nib.load(str(file_out))
+    image_ref = nib.load(str(file_ref))
+
+    report_filename = Path(f"report_{name}.json")
+    out = {}
+    try:
+        assert image_out.header == image_ref.header
+        out["regr"] = "PASSED"
+    except(AssertionError):
+        out["regr"] = "FAILED"
+
+    with report_filename.open("w") as f:
+        json.dump(out, f)
+
+
+if __name__ == '__main__':
+    from argparse import ArgumentParser, RawTextHelpFormatter
+    parser = ArgumentParser(description=__doc__,
+                            formatter_class=RawTextHelpFormatter)
+    parser.add_argument("-out", dest="file_out",
+                        help="file with the output for testing")
+    parser.add_argument("-ref", dest="file_ref",
+                        help="file with the reference output")
+    parser.add_argument("-name", dest="name",
+                        help="name of the test provided by a user")
+    args = parser.parse_args()
+
+    test_afni_scan_header(**vars(args))
--- testkraken/tests/test_afni.py
+++ testkraken/tests/test_afni.py
@@ -4,14 +4,10 @@ import pytest
 from ..testrunner import runner
 
 Workflows_main_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)),
-                                "../../workflows4regtests", "afni",)
+                                "../../workflows4regtests", "afni_dc2019",)
 workflows_list = [os.path.join(Workflows_main_dir, workf) for workf in next(os.walk(Workflows_main_dir))[1]]
 
 @pytest.mark.parametrize("workflow_dir", workflows_list)
 def test_afni_examples(workflow_dir):
     print(workflow_dir)
-    try:
-        runner(workflow_dir)
-    except Exception as e:
-        print(e)
-        assert False
+    runner(workflow_dir)
