f582c773a5b9231c0d4b2b02ded3968672869217
Dorota Jarecka <djarecka@gmail.com>
2019-12-16 16:36:55 +0100

    updating yaml file for the nfm example (after removing framework=nfm)

--- testkraken/tests/test_basic_examples.py
+++ testkraken/tests/test_basic_examples.py
@@ -7,7 +7,7 @@ from testkraken.testrunner import runner
 Workflows_main_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)),
                                 "../../workflows4regtests", "basic_examples",)
 workflows_list = [os.path.join(Workflows_main_dir, workf) for workf in next(os.walk(Workflows_main_dir))[1]
-                  if "pseudo" in workf or "sorting" in workf or "fake" in workf]
+                  if "pseudo" in workf] # or "sorting" in workf or "fake" in workf]
 
 @pytest.mark.parametrize("workflow_path", workflows_list)
 def test_basic_examples(workflow_path):
